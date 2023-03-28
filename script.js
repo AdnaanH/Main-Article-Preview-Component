61902ad2e4fc6574774b90f3e8c3fc4222f00d23
AdnaanH <60857004+AdnaanH@users.noreply.github.com>
2023-03-28 01:17:02 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const share = document.querySelector('.share');
+const popup = document.querySelector('.social-popup')
+
+share.addEventListener('click', ()=>{
+    popup.classList.add('active');
+});
